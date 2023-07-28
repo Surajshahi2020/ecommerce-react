86953eda8c8686b09dfd4487ee8cd51a65e3238d
Surajshahi2020 <suraj.171245@ncit.edu.np>
2023-07-28 12:16:06 +0545

    configure route for home page

---
+++ src/components/maincontent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainContent = () => {
+    return <h1>Main content part</h1>;
+};
+
+export default MainContent;
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/header";
+import MainContent from "../components/maincontent";
+import Footer from "../components/footer";
+
+const HomePage = () => {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+};
+export default HomePage;
